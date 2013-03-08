addb474feb9ea048f24aee0389a896305061fbbd
Abel Amaro Julian <amarotux@gmail.com>
2013-03-08 16:21:18 -0600

    Test de Git-Hub
    Primera vez que uso Git-Hub (no se como funciona jeje :p)

---
+++ RSSGenBDev/Model/Article.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSSGenBDev.Common;
+
+namespace RSSGenBDev.Model
+{
+    public class Article:BindableBase
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _summary;
+        public string Summary
+        {
+            get { return _summary; }
+            set { SetProperty(ref _summary,value); }
+        }
+
+        private string _content;
+        public string Content
+        {
+            get { return _content; }
+            set { SetProperty(ref _content,value); }
+        }
+
+        private Uri _imgUri;
+        public Uri ImgUri
+        {
+            get { return _imgUri; }
+            set { SetProperty(ref _imgUri, value); }
+        }
+    }
+}
---
+++ RSSGenBDev/ViewModel/RssMainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSSGenBDev.Common;
+using RSSGenBDev.Model;
+using RSSGenBDev.Util;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace RSSGenBDev.ViewModel
+{
+    public class RssMainViewModel : BindableBase
+    {
+        private ArticleList _articles;
+
+        public ArticleList Articles
+        {
+            get { return _articles; }
+            set { SetProperty(ref _articles, value); }
+        }
+
+        private string _feedUrlString;
+
+        public string FeedUrlString
+        {
+            get { return _feedUrlString; }
+            set { SetProperty(ref _feedUrlString, value); }
+        }
+
+        public async Task Initialize()
+        {
+            Articles = await RSSHelper.GetArticleListFromFeedAsync(this.FeedUrlString);
+        }
+
+        public async void ViewLoadHandler(object o, RoutedEventArgs e)
+        {
+            var tmpView = o as FrameworkElement;
+            if (tmpView!=null)
+            {
+                tmpView.DataContext = this;
+            }
+            
+            if (!string.IsNullOrWhiteSpace(FeedUrlString))
+                await Initialize();
+        }
+
+
+
+        public static string GetHtmlString(DependencyObject obj)
+        {
+            return (string)obj.GetValue(HtmlStringProperty);
+        }
+
+        public static void SetHtmlString(DependencyObject obj, string value)
+        {
+            obj.SetValue(HtmlStringProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for HtmlString.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HtmlStringProperty =
+            DependencyProperty.RegisterAttached("HtmlString",
+                                    typeof(string), typeof(RssMainViewModel),
+                                    new PropertyMetadata("", HtmlStringChanged));
+
+        public static void HtmlStringChanged(DependencyObject sender, DependencyPropertyChangedEventArgs arg)
+        {
+            var wb = sender as WebView;
+            if (wb != null)
+                wb.NavigateToString((string)arg.NewValue);
+        }
+    }
+}
